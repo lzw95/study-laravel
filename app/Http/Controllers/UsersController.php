a8ea9b0dc1969886c6c79de0ddd40e1c28cc69b2
Liuzw <zcboy95@gmail.com>
2017-09-14 07:06:45 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,15 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Requests;
 
 class UsersController extends Controller
 {
-    
+    // 注册页面
 	public function create()
 	{
 		return view('users.create');
 	}
 
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+			'name'	=>	'required|max:50',
+			'email' =>  'required|email|unique:users|max:255',
+			'password' => 'required|min:6|confirmed' 
+		]);
+
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => bcrypt($request->password)
+			]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+		return redirect()->route('users.show', [$user]);
+	}
+
+	// 个人中心
+	public function show(User $user)
+	{
+		return view('users.show', compact('user'));
+	}
 
 
 }
--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,24 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 // 注册
 Route::get('signup', 'UsersController@create')->name('signup');
 
-Route::get('creaaa', 'UsersController@121')->name('121');
-
-
+Route::resource('users', 'UsersController');
+//等同于下面这些路由
+/**
+// 显示所有用户列表的页面
+Route::get('/users', 'UsersController@index')->name('users.index');
+// 个人页面
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// 创建用户的页面
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+// 创建用户
+Route::post('/users', 'UsersController@store')->name('users.store');
+// 编辑用户个人资料页面
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// 更新用户
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+// 删除用户
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+ */
 
 
 
